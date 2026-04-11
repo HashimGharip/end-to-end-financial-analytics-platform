8c19ab411fcad1b86c617f867490e62d2f51076b
HashimGharip <hgharip21@gmail.com.com>
2026-04-11 22:28:55 +0200

    add indexs on fact

--- dbt_project/financial_analytics_project/models/marts/fact_transactions.sql
+++ dbt_project/financial_analytics_project/models/marts/fact_transactions.sql
@@ -1,6 +1,13 @@
 {{ config(
     materialized='incremental',
-    unique_key='transaction_id'
+    unique_key='transaction_id',
+    post_hook=[
+        "CREATE INDEX IF NOT EXISTS idx_fact_user_id ON {{ this }} (user_id)",
+        "CREATE INDEX IF NOT EXISTS idx_fact_card_id ON {{ this }} (card_id)",
+        "CREATE INDEX IF NOT EXISTS idx_fact_mcc ON {{ this }} (mcc)",
+        "CREATE INDEX IF NOT EXISTS idx_fact_date ON {{ this }} (transaction_date)",
+        "CREATE INDEX IF NOT EXISTS idx_fact_user_date ON {{ this }} (user_id, transaction_date)"
+    ]
 ) }}
 
 WITH base AS (
